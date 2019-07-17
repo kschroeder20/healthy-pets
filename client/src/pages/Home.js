8cd151d63dd5755241805a0b8901f8ff82bf5d28
Kevin Schroeder <schroederkevin94@gmail.com>
2019-07-17 08:35:59 -0400

    added basic login functionality

---
+++ client/src/pages/Home.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { withAuth } from '@okta/okta-react';
+
+export default withAuth(
+    class Home extends Component {
+        state = { authenticated: null };
+
+        checkAuthentication = async () => {
+            const authenticated = await this.props.auth.isAuthenticated();
+            if (authenticated !== this.state.authenticated) {
+                this.setState({ authenticated });
+            }
+        };
+
+        async componentDidMount() {
+            this.checkAuthentication();
+        }
+
+        async componentDidUpdate() {
+            this.checkAuthentication();
+        }
+
+        login = async () => {
+            this.props.auth.login('/');
+        };
+
+        logout = async () => {
+            this.props.auth.logout('/');
+        };
+
+        render() {
+            if (this.state.authenticated === null) return null;
+
+            //if we are authenticated
+            const mainContent = this.state.authenticated ? (
+                <div>
+                    <p className="lead">
+                        You have been logged in!{' '}
+                        <Link to="/profile">click here</Link>
+                    </p>
+                    <button className="btn btn-light btn-lg" onClick={this.logout}>
+                        Logout
+                </button>
+                </div>
+            ) : (
+                    <div>
+                        <p className="lead">
+                            Right now, we have to have someone on the back end manually add users
+                        </p>
+                        <button className="btn btn-dark btn-lg" onClick={this.login}>
+                            Login
+                        </button>
+                    </div>
+                );
+
+            return (
+                <div className="jumbotron">
+                    <h1 className="display-4">Pet Protal</h1>
+                    {mainContent}
+                </div>
+            );
+        }
+    }
+);
