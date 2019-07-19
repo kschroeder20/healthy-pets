a0dd8ffffb883d888de6b787cd01a9d1c5f405da
BShieh <rebeccasshieh@gmail.com>
2019-07-19 12:28:01 -0400

    wip: added modals for owner, pet, and medical info. untested as of yet, but added into corresponding components.

--- client/src/components/MedicalHistory/index.js
+++ client/src/components/MedicalHistory/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./style.css";
+import MedicalModal from "../MedicalModal";
 
 function Medical(props) {
   return (
     <div class="card">
       <div class="card-body">
         <h5 class="card-title">Medical History</h5>
-        <button className="edit-button">Edit</button>
+        <button className="medical-edit-button">Edit</button>
+        <MedicalModal />
       </div>
       <ul class="list-group list-group-flush">
         <li class="list-group-item">Medications:{props.medication}</li>
