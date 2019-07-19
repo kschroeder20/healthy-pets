d49ede8f30e2bd9e51d11fafb528b16d5acd1b19
Kevin Schroeder <schroederkevin94@gmail.com>
2019-07-19 07:45:42 -0400

    pulling

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,101 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
 import Navbar from './components/NavBar';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import Login from './components/Login';
+import React, { Component } from "react"
+import "./App.css"
+import firebase from "firebase"
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
 
-function onAuthRequired({ history }) {
-  history.push('/login')
-}
+firebase.initializeApp({
+  apiKey: "AIzaSyDz36s2csIU48ULaVEKGENh81pCUvYCVBk",
+  authDomain: "healthy-pets-32ee4.firebaseapp.com"
+})
 
 class App extends Component {
+  state = { isSignedIn: false }
+  uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccess: () => false
+    }
+  }
+
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ isSignedIn: !!user })
+      console.log("user", user)
+    })
+  }
+
   render() {
     return (
       <Router>
-        <Security
-          issuer="https://dev-217893.okta.com/oauth2/default"
-          client_id="0oax790rjgenQw6O3356"
-          redirect_uri={window.location.origin + '/implicit/callback'}
-          onAuthRequired={onAuthRequired}
-        >
-          <div className="App">
-            <Navbar />
-            <div className="container">
-              <Route path="/" exact={true} component={Home} />
-              <SecureRoute path="/profile" exact={true} component={Profile} />
-              <Route
-                path="/login"
-                render={() => (
-                  <Login baseUrl="https://dev-217893.okta.com" />
-                )}
+        <div className="App">
+          {this.state.isSignedIn ? (
+            <span>
+              <Navbar />
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/profile" component={Profile} />
+                <Route path="/login" component={Login} />
+                {/* <button onClick={() => firebase.auth().signOut()}>Sign out!</button> */}
+                <Route component={Home} />
+              </Switch>
+            </span>
+          ) : (
+              <StyledFirebaseAuth
+                uiConfig={this.uiConfig}
+                firebaseAuth={firebase.auth()}
               />
-              <Route path="/implicit/callback" component={ImplicitCallback} />
-            </div>
-          </div>
-        </Security>
+            )}
+        </div>
       </Router>
-    );
+    )
   }
 }
 
-export default App;
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // render() {
+  //   return (
+  //     <Router>
+  //         <div className="App">
+  //           <Navbar />
+  //           <div className="container">
+  //             <Route path="/" exact={true} component={Home} />
+  //             <SecureRoute path="/profile" exact={true} component={Profile} />
+  //             <Route
+  //               path="/login"
+  //               render={() => (
+  //                 <Login baseUrl="https://dev-217893.okta.com" />
+  //               )}
+  //             />
+  //             <Route path="/implicit/callback" component={ImplicitCallback} />
+  //           </div>
+  //         </div>
+  //     </Router>
+  //   );
+  // }
+// }
+
+// export default App;
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -5,42 +5,42 @@ import Medical from "../components/MedicalHistory";
 import { Container, Row, Col } from "react-grid-system";
 
 class Profile extends Component {
-  state = {
-    currentUserName: "",
-    currentUserEmail: ""
-  };
+    state = {
+        currentUserName: "",
+        currentUserEmail: ""
+    };
 
-  componentDidMount() {
-    const idToken = JSON.parse(localStorage.getItem("okta-token-storage"));
-    this.setState({
-      currentUserEmail: idToken.idToken.claims.email,
-      currentUserName: idToken.idToken.claims.name
-    });
-  }
-  render() {
-    const { currentUserEmail, currentUserName } = this.state;
-    return (
-      <div>
-        <h1>Welcome {currentUserName}</h1>
-        <p>Email: {currentUserEmail}</p>
-        <p>This is where we are going to put all of our pet information</p>
-        <div>
-          <Container>
-            <Row>
-              <Col sm={4}>
-                  <OwnerInfo/>
-                  <Medical/>
-              </Col>
-              <Col sm={4}></Col>
-              <Col sm={4}>
-                  <PetInfo/>
-              </Col>
-            </Row>
-          </Container>
-        </div>
-      </div>
-    );
-  }
+    //   componentDidMount() {
+    //     const idToken = JSON.parse(localStorage.getItem("okta-token-storage"));
+    //     this.setState({
+    //       currentUserEmail: idToken.idToken.claims.email,
+    //       currentUserName: idToken.idToken.claims.name
+    //     });
+    //   }
+    render() {
+        const { currentUserEmail, currentUserName } = this.state;
+        return (
+            <div>
+                <h1>Welcome {currentUserName}</h1>
+                <p>Email: {currentUserEmail}</p>
+                <p>This is where we are going to put all of our pet information</p>
+                <div>
+                    <Container>
+                        <Row>
+                            <Col sm={4}>
+                                <OwnerInfo />
+                                <Medical />
+                            </Col>
+                            <Col sm={4}></Col>
+                            <Col sm={4}>
+                                <PetInfo />
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Profile;
