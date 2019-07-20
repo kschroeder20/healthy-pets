657f5c94005238c875e13d292ddaf3fb840c2bf7
BShieh <rebeccasshieh@gmail.com>
2019-07-20 14:00:57 -0400

    wip: added button styling and overlay details to modals

--- client/src/components/Modals/MedicalModal/index.js
+++ client/src/components/Modals/MedicalModal/index.js
@@ -1,16 +1,26 @@
 import React, {Component} from 'react';
 import Modal from 'react-modal';
+import './style.css';
  
 const customStyles = {
-  content : {
-    top                   : '50%',
-    left                  : '50%',
-    right                 : 'auto',
-    bottom                : 'auto',
-    marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)',
-    width                 : '40rem',
-    height                : '40rem'
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    width: '40rem',
+    height: '40rem',
+    borderRadius: '10px'
+  },
+  overlay: {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgb(8, 5, 145, 0.6)'
   }
 };
  
@@ -28,6 +38,7 @@ class MedicalModal extends Component {
     this.openModal = this.openModal.bind(this);
     this.afterOpenModal = this.afterOpenModal.bind(this);
     this.closeModal = this.closeModal.bind(this);
+    this.saveInfo = this.saveInfo.bind(this);
   }
  
   openModal() {
@@ -36,17 +47,21 @@ class MedicalModal extends Component {
  
   afterOpenModal() {
     // references are now sync'd and can be accessed.
-    this.subtitle.style.color = '#f00';
+    this.subtitle.style.color = 'rgb(8, 5, 145)';
   }
  
   closeModal() {
     this.setState({modalIsOpen: false});
   }
+
+  saveInfo() {
+    console.log("info is saved");
+  }
  
   render() {
     return (
       <div>
-        <button onClick={this.openModal}>Edit</button>
+        <button className="edit" onClick={this.openModal}>Edit</button>
         <Modal
           isOpen={this.state.modalIsOpen}
           onAfterOpen={this.afterOpenModal}
@@ -55,8 +70,7 @@ class MedicalModal extends Component {
           contentLabel="Medical Modal"
         >
  
-          <h2 ref={subtitle => this.subtitle = subtitle}>Medical History</h2>
-          <button onClick={this.closeModal}>close</button>
+          <h2 class="text-center" ref={subtitle => this.subtitle = subtitle}>Medical History</h2>
           <form>
                 <div className="form-group">
                     <label for="medication">Medication:</label>
@@ -78,7 +92,8 @@ class MedicalModal extends Component {
                     <label for="procedures">Procedures</label>
                     <input type="text" className="form-control" id="procedures" placeholder="Procedures"/>
                 </div>
-                <button type="submit" class="btn btn-primary">Save</button>
+                <button type="submit" class="btn btn-primary" onClick={this.saveInfo}>Save</button>
+                <button class="btn btn-danger" onClick={this.closeModal}>Close</button>
           </form>
         </Modal>
       </div>
--- client/src/components/Modals/OwnerModal/index.js
+++ client/src/components/Modals/OwnerModal/index.js
@@ -11,6 +11,14 @@ const customStyles = {
     transform             : 'translate(-50%, -50%)',
     width                 : '40rem',
     height                : '40rem'
+  },
+  overlay: {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgb(8, 5, 145, 0.6)'
   }
 };
  
@@ -36,7 +44,7 @@ class OwnerModal extends Component {
  
   afterOpenModal() {
     // references are now sync'd and can be accessed.
-    this.subtitle.style.color = '#f00';
+    this.subtitle.style.color = 'rgb(8, 5, 145)';
   }
  
   closeModal() {
@@ -46,7 +54,7 @@ class OwnerModal extends Component {
   render() {
     return (
       <div>
-        <button onClick={this.openModal}>Edit</button>
+        <button className="edit" onClick={this.openModal}>Edit</button>
         <Modal
           isOpen={this.state.modalIsOpen}
           onAfterOpen={this.afterOpenModal}
@@ -55,8 +63,7 @@ class OwnerModal extends Component {
           contentLabel="Owner Modal"
         >
  
-          <h2 ref={subtitle => this.subtitle = subtitle}>Owner Information</h2>
-          <button onClick={this.closeModal}>close</button>
+          <h2 class="text-center" ref={subtitle => this.subtitle = subtitle}>Owner Information</h2>
           <form>
                 <div className="form-group">
                     <label for="ownerName">Owner Name:</label>
@@ -87,6 +94,7 @@ class OwnerModal extends Component {
                     <input type="tel" className="form-control" id="vetPhone" placeholder="###-###-####"/>
                 </div>
                 <button type="submit" class="btn btn-primary">Save</button>
+                <button class="btn btn-danger" onClick={this.closeModal}>Close</button>
           </form>
         </Modal>
       </div>
