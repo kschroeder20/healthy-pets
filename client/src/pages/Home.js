ba3da9a2aa6386c1f6b1079ee55fab1011fc478f
stravis11 <stevetravis11@gmail.com>
2019-07-20 08:31:12 -0400

    removed picture component from home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -36,7 +36,6 @@ class Home extends Component {
         <Title />
         <div className="jumbotron text-center">
           <h1 className="display-4">Pet Portal</h1>
-          <Picture />
         </div>
       </div>
     );
