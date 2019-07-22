7f4591af875a62395742a1a9532f2a02467ac7a0
stravis11 <stevetravis11@gmail.com>
2019-07-22 10:25:09 -0400

    Pushing homepage changes again

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,35 +1,32 @@
-
 import React, { Component } from "react";
 // import { Link } from "react-router-dom";
 // import { withAuth } from "@okta/okta-react";
 // import Title from "../components/Title";
 // import NavBar from '../components/NavBar';
 // import PetNav from "../components/PetNav";
-import Title from "../components/Title"
-import NavBar from '../components/NavBar';
-import Picture from "../components/Picture";
-import Calendar from "../components/Calendar";
+import Title from "../components/Title";
+import NavBar from "../components/NavBar";
+import PetCarousel from "../components/Carousel";
+import "./style.css";
 
 // rt default withAuth(
 class Home extends Component {
-  state = { isSignedIn: true }
+  state = { isSignedIn: true };
 
   componentDidMount = () => {
-    const userSignedIn = window.sessionStorage.getItem('userSingedIn');
-    this.setState({ isSignedIn: userSignedIn })
-  }
-  
- render() {
+    const userSignedIn = window.sessionStorage.getItem("userSingedIn");
+    this.setState({ isSignedIn: userSignedIn });
+  };
+
+  render() {
     return (
       <div>
         <NavBar />
         <Title />
-        <div className="jumbotron text-center">
-          <h1 className="display-4">Pet Portal</h1>
-        </div>
+        <PetCarousel />
       </div>
     );
   }
 }
 
-export default Home
+export default Home;
