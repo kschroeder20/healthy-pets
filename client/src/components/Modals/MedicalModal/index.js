5f5bed41cee27ec2be90a7cd2c8c8bde42c983eb
Kevin Schroeder <schroederkevin94@gmail.com>
2019-07-23 08:53:39 -0400

    testing endpoints WIP

--- client/src/components/Modals/MedicalModal/index.js
+++ client/src/components/Modals/MedicalModal/index.js
@@ -1,7 +1,7 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Modal from 'react-modal';
 import './style.css';
- 
+
 const customStyles = {
   content: {
     top: '50%',
@@ -23,14 +23,14 @@ const customStyles = {
     backgroundColor: 'rgb(8, 5, 145, 0.6)'
   }
 };
- 
+
 // Make sure to bind modal to your appElement (http://reactcommunity.org/react-modal/accessibility/)
-Modal.setAppElement('#root');
- 
+Modal.setAppElement(document.getElementById('root'));
+
 class MedicalModal extends Component {
   constructor() {
     super();
- 
+
     this.state = {
       modalIsOpen: false,
       medication: '',
@@ -39,7 +39,7 @@ class MedicalModal extends Component {
       food: '',
       procedures: ''
     };
- 
+
     this.openModal = this.openModal.bind(this);
     this.afterOpenModal = this.afterOpenModal.bind(this);
     this.closeModal = this.closeModal.bind(this);
@@ -49,46 +49,46 @@ class MedicalModal extends Component {
     this.changeFood = this.changeFood.bind(this);
     this.changeProcedures = this.changeProcedures.bind(this);
   }
- 
+
   openModal() {
-    this.setState({modalIsOpen: true});
+    this.setState({ modalIsOpen: true });
   }
- 
+
   afterOpenModal() {
     // references are now sync'd and can be accessed.
     this.subtitle.style.color = 'rgb(8, 5, 145)';
   }
- 
+
   closeModal() {
-    this.setState({modalIsOpen: false});
+    this.setState({ modalIsOpen: false });
   }
 
   changeMedication(event) {
     console.log("medication is saved");
-    this.setState({medication: event.target.value});
+    this.setState({ medication: event.target.value });
   }
 
   changeVaccines(event) {
     console.log("vaccine is saved");
-    this.setState({vaccines: event.target.value});
+    this.setState({ vaccines: event.target.value });
   }
 
   changeAllergies(event) {
     console.log("allergies are saved");
-    this.setState({allergies: event.target.value});
+    this.setState({ allergies: event.target.value });
   }
 
   changeFood(event) {
     console.log("food is saved");
-    this.setState({food: event.target.value});
+    this.setState({ food: event.target.value });
   }
 
   changeProcedures(event) {
     console.log("procedures are saved");
-    this.setState({procedures: event.target.value});
+    this.setState({ procedures: event.target.value });
   }
 
-  handleSubmit (event) {
+  handleSubmit(event) {
     event.preventDefault();
     // this.changeMedication();
     // this.changeAllergies();
@@ -96,7 +96,7 @@ class MedicalModal extends Component {
     // this.changeVaccines();
     // this.changeProcedures();
   }
- 
+
   render() {
     return (
       <div>
@@ -108,26 +108,26 @@ class MedicalModal extends Component {
           style={customStyles}
           contentLabel="Medical Modal"
         >
- 
+
           <h2 className="text-center" ref={subtitle => this.subtitle = subtitle}>Medical History</h2>
           <form onSubmit={this.handleSubmit}>
-                <div className="form-group">
-                    <input type="text" className="form-control" value={this.state.medication} onChange={this.changeMedication} placeholder="Medication List"/>
-                </div>
-                <div className="form-group">
-                    <input type="text" className="form-control" value={this.state.vaccines} onChange={this.changeVaccines} placeholder="Vaccines"/>
-                </div>
-                <div className="form-group">
-                    <input type="text" className="form-control" value={this.state.allergies} onChange={this.changeAllergies} placeholder="Allergies"/>
-                </div>
-                <div className="form-group">
-                    <input type="text" className="form-control" value={this.state.food} onChange={this.changeFood} placeholder="Food"/>
-                </div>
-                <div className="form-group">
-                    <input type="text" className="form-control" value={this.state.procedures} onChange={this.changeProcedures} placeholder="Procedures"/>
-                </div>
-                <input type="submit" value="Submit" color="primary" className="btn btn-primary" />
-                <button className="btn btn-danger" onClick={this.closeModal}>Close</button>
+            <div className="form-group">
+              <input type="text" className="form-control" value={this.state.medication} onChange={this.changeMedication} placeholder="Medication List" />
+            </div>
+            <div className="form-group">
+              <input type="text" className="form-control" value={this.state.vaccines} onChange={this.changeVaccines} placeholder="Vaccines" />
+            </div>
+            <div className="form-group">
+              <input type="text" className="form-control" value={this.state.allergies} onChange={this.changeAllergies} placeholder="Allergies" />
+            </div>
+            <div className="form-group">
+              <input type="text" className="form-control" value={this.state.food} onChange={this.changeFood} placeholder="Food" />
+            </div>
+            <div className="form-group">
+              <input type="text" className="form-control" value={this.state.procedures} onChange={this.changeProcedures} placeholder="Procedures" />
+            </div>
+            <input type="submit" value="Submit" color="primary" className="btn btn-primary" />
+            <button className="btn btn-danger" onClick={this.closeModal}>Close</button>
           </form>
         </Modal>
       </div>
