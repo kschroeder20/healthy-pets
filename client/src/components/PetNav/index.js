24292b1035dd3780da3642805e93d580f9a556eb
BShieh <rebeccasshieh@gmail.com>
2019-07-27 11:04:24 -0400

    wip: added datepicker calendar and pet nav to profile page.

--- client/src/components/PetNav/index.js
+++ client/src/components/PetNav/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Dropdown, Button } from "react-bootstrap";
 import { FaPlusSquare, FaShareAlt } from "react-icons/fa";
+import "./style.css";
 
 const PetNav = function() {
   return (
-    <nav className="navbar">
+    <nav className="navbar" id="pet-nav">
       <div className="container">
         <Dropdown>
           <Dropdown.Toggle variant="secondary" id="dropdown-basic">
@@ -12,9 +13,9 @@ const PetNav = function() {
           </Dropdown.Toggle>
 
           <Dropdown.Menu>
-            <Dropdown.Item href="#/action-1">Storm</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">Jack</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Lady</Dropdown.Item>
+            <Dropdown.Item href="#/action-1">Pet 1</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Pet 2</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Pet 3</Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
 
