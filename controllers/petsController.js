a4707a8bbfac0d392e1421f733cfd412df8dd334
stravis11 <stevetravis11@gmail.com>
2019-07-29 21:05:28 -0400

    Updated model and controller to include pet picture (petUrl). Updated seedDB to include petUrl.

--- controllers/petsController.js
+++ controllers/petsController.js
@@ -1,21 +1,18 @@
 const db = require("../models");
-const fs = require('fs');
-
+const fs = require("fs");
 
 // Defining methods for the booksController
 module.exports = {
-    findAll: function (req, res) {
-        db.Pet
-            .find(req.query)
-            .sort({ date: -1 })
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.status(422).json(err));
-    },
-    findById: function (req, res) {
-        db.Pet
-            .find({ uid: req.params.id })
-            .then(dbModel => {
-                let petInfo = `
+  findAll: function(req, res) {
+    db.Pet.find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.Pet.find({ uid: req.params.id })
+      .then(dbModel => {
+        let petInfo = `
                 Owner Name: ${dbModel[0].ownerName}
                 Home Phone: ${dbModel[0].homePhone}
                 Mobile Phone: ${dbModel[0].mobilePhone}
@@ -34,6 +31,7 @@ module.exports = {
                 Pet Breed:${dbModel[0].petBreed}
                 Pet Sex:${dbModel[0].petSex}
                 Pet Weight:${dbModel[0].petWeight}
+                Pet Picture:${dbModel[0].petUrl}
 
                 Medications: ${dbModel[0].petMedications}
                 Inoculations: ${dbModel[0].petInoculations}
@@ -42,55 +40,51 @@ module.exports = {
 
                 Rabies Tage: ${dbModel[0].petRabiesTag}
                 Microchip: ${dbModel[0].petMicrochip}
-                `
-                fs.writeFile('pdfInfo.txt',petInfo, function (err) {
-                    if (err) throw err;
-                    console.log('Saved!');
-                  });
-                res.json(dbModel)
-            })
-            .catch(err => {
-                console.log(err);
-                return res.status(422).json(err)
-            });
-    },
-    create: function (req, res) {
-        console.log(req.body)
-        db.Pet
-            .create(req.body)
-            .then(dbModel => {
-                res.json(dbModel)
-            })
-            .catch(err => res.status(422).json(err));
-    },
-    update: function (req, res) {
-        db.Pet
-            .findOneAndUpdate({ uid: req.body.userId }, req.body)
-            .then(dbModel => res.json(dbModel))
-            .catch(err => {
-                console.log(err);
-                return res.status(422).json(err);
-            });
-    },
-    remove: function (req, res) {
-        db.Pet
-            .findById({ _id: req.params.id })
-            .then(dbModel => dbModel.remove())
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.status(422).json(err));
-    },
-    downloadPDF: function (req, res) {
-        var spawn = require('child_process').spawn;
-        var child = spawn('ruby', ['pdf.rb']);
-        var pdf = '';
-        child.stdout.on('data', function(data){
-            console.log("HERE");
-            pdf = data;
+                `;
+        fs.writeFile("pdfInfo.txt", petInfo, function(err) {
+          if (err) throw err;
+          console.log("Saved!");
         });
-        child.on('exit', function(code){
-                res.setHeader('Content-Type', 'application/text');
-                res.send(pdf);
-        });
-          
-    }
+        res.json(dbModel);
+      })
+      .catch(err => {
+        console.log(err);
+        return res.status(422).json(err);
+      });
+  },
+  create: function(req, res) {
+    console.log(req.body);
+    db.Pet.create(req.body)
+      .then(dbModel => {
+        res.json(dbModel);
+      })
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    db.Pet.findOneAndUpdate({ uid: req.body.userId }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => {
+        console.log(err);
+        return res.status(422).json(err);
+      });
+  },
+  remove: function(req, res) {
+    db.Pet.findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  downloadPDF: function(req, res) {
+    var spawn = require("child_process").spawn;
+    var child = spawn("ruby", ["pdf.rb"]);
+    var pdf = "";
+    child.stdout.on("data", function(data) {
+      console.log("HERE");
+      pdf = data;
+    });
+    child.on("exit", function(code) {
+      res.setHeader("Content-Type", "application/text");
+      res.send(pdf);
+    });
+  }
 };
